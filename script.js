8b24fb19d0cb1e0b4f77428ed4c4bd0b7f57235d
Richa Biswas <richabiswas26@gmail.com>
2025-08-03 00:53:16 +0530

    Update sent message in script file

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ tailwind.config = {
             
             // In a real application, you would send this data to a server
             // For demo purposes, we'll just show an alert
-            alert(`Thank you, ${name}! Your message has been received. I'll get back to you soon.`);
+            alert(`Thank you, ${name}! email will not be sent .\n\nSubject: ${subject}\nMessage: ${message}`);
             
             // Reset form
             contactForm.reset();
@@ -185,7 +185,7 @@ tailwind.config = {
             }
         }
         
-        
+
         createBackgroundElements();
         
         // Download Resume Button
